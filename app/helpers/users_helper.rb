70637f0afd699929b6621a8a3768b238ffc61cbd
Emily <emily.cm.harvey@gmail.com>
2011-06-02 11:30:03 -0600

    User signup complete

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,6 +1,10 @@
 module UsersHelper
+  def email_address(user)
+    user.email
+  end
+  
   def gravatar_for(user, options = { :size => 50 })
-    gravatar_image_tag(user.email.downcase, :alt => user.name,
+    gravatar_image_tag(email_address(user).downcase, :alt => user.name,
                                             :class => 'gravatar',
                                             :gravatar => options)
   end
