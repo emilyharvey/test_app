dbeaff975c08ed8d775a44fca81badb402b96513
Emily <emily.cm.harvey@gmail.com>
2011-06-02 10:14:11 -0600

    User model with passwords and shwo page

---
+++ spec/factories.rb
@@ -0,0 +1,7 @@
+# By using the symbol ':user', we get Factory Girl to simulate the User model.
+Factory.define :user do |user|
+  user.name                  "Emily Harvey"
+  user.email                 "emilysemail@email.com"
+  user.password              "maestro"
+  user.password_confirmation "maestro"
+end
